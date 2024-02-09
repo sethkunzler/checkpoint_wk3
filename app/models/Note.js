a96e07779e1fbad3820e36e43fd8669372078ead
Seth Kunzler <sethkunzler@gmail.com>
2024-02-09 16:34:52 -0700

    Stubbed out HTML

--- app/models/Note.js
+++ app/models/Note.js
@@ -13,14 +13,30 @@ export class Note {
   
   get NoteListHTMLTemplate() {
     return /*html*/ `
-  <!-- <p id="${this.id}">Title ${this.name}<span class="${this.color}"></span></p> -->
+  <p id="${this.id}" onclick="setActiveNote()" class="selectable rounded p-2" role="button">${this.name}</p>
   `
 }
 
 get ActiveNoteHTMLTemplate() {
   return /*html*/ `
-  <!-- <h1>Title: ${this.name}</h1> -->
-  <!-- <textarea name="body" id="noteInformation" cols="30" rows="10">${this.body}</textarea> -->
+  <section class="container">
+          <div class="row">
+            <div class="col-12 d-flex justify-content-between p-4">
+              <h1>Title: ${this.name} <span class="${this.color}"></span></h1>
+              <button type="button" class="btn btn-danger shadow px-3">x</button>
+            </div>  
+            <div class="col-4 p-4">
+              <p>Created at: ${this.timeCreated}</p>
+              <p>Updated at: ${this.timeUpdated}</p>
+              <p>Words: 0; Characters: 0</p>
+              <button type="button" class="btn btn-outline-danger shadow">Delete</button>
+              <button type="button" class="btn btn-success shadow mx-3">Save</button>
+            </div>
+            <div class="col-md-8">
+              <textarea class="m-4 p-3 bg-gray-light rounded shadow" name="body" id="noteInformation" cols="50" rows="15"></textarea> 
+            </div>
+          </div>
+        </section>
   `
 }
 }
