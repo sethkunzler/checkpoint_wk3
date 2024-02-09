98b0f3d2c1ff9719fb81622f75fa73b614001644
Seth Kunzler <sethkunzler@gmail.com>
2024-02-09 11:28:52 -0700

    first commit

---
+++ app/router-config.js
@@ -0,0 +1,21 @@
+import { Router } from "./utils/Router.js";
+
+
+export const router = new Router([
+  {
+    path: '',
+    controllers: [],
+    view: /*html*/`
+    <div class="bg-white p-3">
+      <div class="card-body">
+        <p>Home Page</p>
+        <button class="btn btn-dark" onclick="app.HomeController.testButton()">😎</button>
+      </div>
+    </div>
+    `
+  }
+  // {
+  //   path: '#/about',
+  //   view: 'app/views/AboutView.html'
+  // }
+])
